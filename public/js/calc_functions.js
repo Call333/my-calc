8cb0e599b7192c615287f16a9ae8c27b24beaa23
Calebe <cam4@aluno.ifal.edu.br>
2022-05-05 14:31:49 -0300

    Deixando com raiz quadrada

--- public/js/calc_functions.js
+++ public/js/calc_functions.js
@@ -1,6 +1,7 @@
+// Buttuons
 function atualizarDisplay(btn){
     const display = document.getElementById('display');
-    if(display.value.length === 9) return;
+    if(display.value.length === 18) return;
     if(display.value === '0') display.value = btn.value;
     else display.value += btn.value;
 }
@@ -11,20 +12,21 @@ function limparDisplay(){
 
 var operador = ''
 var valor1 = 0
+
 function atualizarOperacao(btn){
     const display = document.getElementById('display');
-    operador = btn.value;
-    valor1 = parseInt(display.value);
+    operador = btn.getAttribute('operacao');
+    valor1 = parseFloat(display.value);
     display.value = '0';
 }
-
+// =
 function calcularOperacao(){
     const display = document.getElementById('display');
-    const valor2 = parseInt(display.value);
+    const valor2 = parseFloat(display.value);
     valor1 = eval(valor1+operador+valor2);
     display.value = valor1;
     operador = '';
-}       
+}
 
 function manipularTeclado(){
     if(/[0-9]/.test(event.key))
